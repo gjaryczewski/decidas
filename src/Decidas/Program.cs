a49ccd631679fbf46b4ff38b6ed44a3cab1dea21
Gerard Jaryczewski <gjaryczewski@gmail.com>
2024-04-25 23:58:19 +0200

    Restart to final approach

--- src/Decidas/Program.cs
+++ src/Decidas/Program.cs
@@ -1,57 +1,20 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.EntityFrameworkCore;
-
 namespace Decidas;
 
-public partial class Program
+public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddRazorPages(options =>
-        {
-            options.Conventions.AllowAnonymousToPage("/");
-            options.Conventions.AuthorizeFolder("/Accounts");
-            options.Conventions.AllowAnonymousToPage("/Accounts/Login");
-            options.Conventions.AllowAnonymousToPage("/Accounts/Logout");
-            options.Conventions.AllowAnonymousToPage("/Accounts/Register");
-            options.Conventions.AuthorizeFolder("/Gatherings");
-            options.Conventions.AuthorizeFolder("/Groups");
-            options.Conventions.AuthorizeFolder("/Keepers");
-            options.Conventions.AuthorizeFolder("/Members");
-            options.Conventions.AuthorizeFolder("/Topics");
-            options.Conventions.AllowAnonymousToFolder("/Contact");
-        });
-        
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddDbContext<MainDbContext>(
-            options => options.UseSqlServer("name=ConnectionStrings:MainDb"));
-
-        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie();
-
-        builder.Services.AddHttpContextAccessor();
-
-        builder.Services.Configure<RouteOptions>(options =>
-        {
-            options.LowercaseUrls = true;
-            options.LowercaseQueryStrings = true;
-        });
+        builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
         {
-            using (var scope = app.Services.CreateScope())
-            {
-                var service = scope.ServiceProvider;
-                var context = service.GetService<MainDbContext>();
-                Data.DevelopmentInitialData.Publish(context);
-            }
+            app.UseDeveloperExceptionPage();
         }
-
-        if (!app.Environment.IsDevelopment())
+        else
         {
             app.UseExceptionHandler("/Error");
             app.UseHsts();
@@ -60,11 +23,11 @@ public partial class Program
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
-        app.UseAuthentication();
+        app.UseRouting();
+
         app.UseAuthorization();
 
         app.MapRazorPages();
-        app.MapDefaultControllerRoute();
 
         app.Run();
     }
